0a1138c93880257e536264325b29b499905cb3e5
18047096 <18047096@cnsuning.com>
2019-06-19 10:30:31 +0800

    初始化项目

---
+++ iOS-Engineering/iOS-Engineering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Engineering
+//
+//  Created by sn_zjs on 2019/4/17.
+//  Copyright © 2019 sn_zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
