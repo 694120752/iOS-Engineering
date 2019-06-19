8f0965e76dd94bfbb001d1bfe5dc091eb65ec617
Zjs <694120752@qq.com>
2019-06-19 10:30:31 +0800

    初始化项目

---
+++ iOS-Engineering/iOS-Engineering/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS-Engineering
+//
+//  Created by sn_zjs on 2019/4/17.
+//  Copyright © 2019 sn_zjs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ iOS-Engineering/iOS-Engineering/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-Engineering
+//
+//  Created by sn_zjs on 2019/4/17.
+//  Copyright © 2019 sn_zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
